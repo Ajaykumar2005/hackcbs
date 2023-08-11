a2b9cb0e5debf1d1a93c23291f443fbe6076f19f
Ajaykumar2005 <98280155+Ajaykumar2005@users.noreply.github.com>
2023-08-11 18:37:08 +0530

    shadow drop

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -1,25 +1,28 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-    const nightElement = document.getElementById('night');
-    const numStars = 170;
+  const nightElement = document.getElementById('night');
+  const numStars = 100; // Adjust the number of stars as needed
 
-    for (let i = 0; i < numStars; i++) {
-      const shootingStar = document.createElement('div');
-      shootingStar.classList.add('shooting_star');
-      nightElement.appendChild(shootingStar);
-    }
-  });
-  document.addEventListener('DOMContentLoaded', function() {
-const shootingStars = document.querySelectorAll('.shooting_star');
+  function positionStars() {
+    const shootingStars = document.querySelectorAll('.shooting_star');
 
-shootingStars.forEach((star, index) => {
-const randomTop = Math.random() * window.innerHeight;
-const randomLeft = Math.random() * window.innerWidth;
+    shootingStars.forEach(star => {
+      const randomTop = Math.random() * window.innerHeight-5;
+      const randomLeft = Math.random() * window.innerWidth-5;
 
-star.style.top = randomTop + 'px';
-star.style.left = randomLeft + 'px';
-star.style.animationDelay = index * 1000 + 'ms';
-});
+      star.style.top = randomTop + 'px';
+      star.style.left = randomLeft + 'px';
+    });
+  }
 
-// Rest of your JavaScript code...
+  for (let i = 0; i < numStars; i++) {
+    const shootingStar = document.createElement('div');
+    shootingStar.classList.add('shooting_star');
+    nightElement.appendChild(shootingStar);
+  }
+
+  // Initial positioning of stars
+  positionStars();
+
+  // Reposition stars on window resize
+  window.addEventListener('resize', positionStars);
 });
